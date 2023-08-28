9d02fdc4494d32a986c68a91c290687392027d41
Diego Rojas <134979085+Drojasva@users.noreply.github.com>
2023-08-28 16:38:54 -0400

    Update script.js
    se agregaron los iconos y se arreglo el js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,70 @@
+     const propiedades_alquiler = [
+            {
+                nombre: 'Property A',
+                src: 'path/to/image1.jpg',
+                descripcion: 'Description of property A.',
+                ubicacion: 'Location A',
+                habitaciones: 3,
+                costo: 1200,
+                smoke: false,
+                pets: true
+            },
+            // Más propiedades en alquiler...
+        ];
 
-const propiedades_alquiler = [
-  {
-    nombre: 'Property A',
-    src: 'path/to/image1.jpg',
-    descripcion: 'Description of property A.',
-    ubicacion: 'Location A',
-    habitaciones: 3,
-    costo: 1200,
-    smoke: false,
-    pets: true
-  },
-  // ... add more properties for rent
-];
+        const propiedades_venta = [
+            {
+                nombre: 'Property A',
+                src: 'path/to/image1.jpg',
+                descripcion: 'Description of property A.',
+                ubicacion: 'Location A',
+                habitaciones: 3,
+                costo: 1200,
+                smoke: false,
+                pets: true
+            },
+            // Más propiedades en venta...
+        ];
 
-const propiedades_venta = [
-  {
-    nombre: 'Property X',
-    src: 'path/to/image2.jpg',
-    descripcion: 'Description of property X.',
-    ubicacion: 'Location X',
-    habitaciones: 4,
-    costo: 250000,
-    smoke: true,
-    pets: false
-  },
-  // ... add more properties for sale
-];
+        function generarContenidoPropiedad(property) {
+            return `
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="${property.src}" class="card-img-top" alt="Imagen de propiedad" />
+                        <div class="card-body">
+                            <h5 class="card-title">${property.nombre}</h5>
+                            <p class="card-text">${property.descripcion}</p>
+                            <p><i class="fas fa-map-marker-alt"></i> ${property.ubicacion}</p>
+                            <p><i class="fas fa-bed"></i> ${property.habitaciones} |
+                                <i class="fas fa-bath"></i> ${property.habitaciones}
+                            </p>
+                            <p><i class="fas fa-dollar-sign"></i> ${property.costo}</p>
+                            <p class="${property.smoke ? 'text-success' : 'text-danger'}">
+                                <i class="fas ${property.smoke ? 'fa-smoking' : 'fa-smoking-ban'}"></i>
+                                ${property.smoke ? 'Permitido fumar' : 'No se permite fumar'}
+                            </p>
+                            <p class="${property.pets ? 'text-success' : 'text-danger'}">
+                                <i class="fas fa-paw"></i>
+                                ${property.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
 
-  const propertyContainer = document.querySelector('.property-container');
+        const propertyContainer = document.querySelector('.property-container');
+        if (propertyContainer) {
+            propiedades_venta.forEach(property => {
+                const propertyTemplate = generarContenidoPropiedad(property);
+                propertyContainer.innerHTML += propertyTemplate;
+            });
+        }
 
-// For properties for sale
-propiedades_venta.forEach(property => {
-  const propertyTemplate = `
-    <div class="col-md-4">
-      <div class="property">
-        <img src="${property.src}" alt="${property.nombre}" />
-        <h3>${property.nombre}</h3>
-        <p>${property.descripcion}</p>
-        <!-- Other property details -->
-      </div>
-    </div>
-  `;
-  propertyContainer.innerHTML += propertyTemplate;
-});
-
-// For properties for rent
-propiedades_alquiler.forEach(property => {
-  const propertyTemplate = `
-    <div class="col-md-4">
-      <div class="property">
-        <img src="${property.src}" alt="${property.nombre}" />
-        <h3>${property.nombre}</h3>
-        <p>${property.descripcion}</p>
-        <!-- Other property details -->
-      </div>
-    </div>
-  `;
-  propertyContainer.innerHTML += propertyTemplate;
-});
-
-${property.smoke ? 'Smoking Allowed' : 'No Smoking'}
-${property.pets ? 'Pets Allowed' : 'No Pets Allowed'}
-
-  
-  // Generar tarjetas de propiedades en venta y alquiler
- //  generarTarjetas(propiedadesVenta, 'venta');
- //  generarTarjetas(propiedadesAlquiler, 'alquiler');
-  
+        const propertyContainerRent = document.querySelector('.property-container-rent');
+        if (propertyContainerRent) {
+            propiedades_alquiler.forEach(property => {
+                const propertyTemplate = generarContenidoPropiedad(property);
+                propertyContainerRent.innerHTML += propertyTemplate;
+            });
+        }
